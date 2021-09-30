5a9c66cd8a870b1bc500b77e43d2f4eee218d589
SagorMiah <allsamiullah304@gmail.com>
2021-10-01 03:03:13 +0600

    spotify clone

---
+++ src/components/Login.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../css/login.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <img
+        src="https://getheavy.com/wp-content/uploads/2019/12/spofify2019-830x350.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Login;
